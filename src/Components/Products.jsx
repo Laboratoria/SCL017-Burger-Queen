2837d4864af62eb50f2f9d0b962d8d485d31d39b
natalianancupil <n.nancupil.v@gmail.com>
2021-10-07 09:17:45 -0300

    css products

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -7,12 +7,12 @@ export default function Products({products = []}){
 
   const renderCard = (item, index) => {
     return ( 
-                 <Card className="boxCard" key={index}>
+                 <Card className="boxCard" key={index} style={{padding: '5px'}}>
                   <CardImg className="imgCard" src={item.image} alt="Card image cap" style={{width: "100px", height:"100px"}}/>
-                  <CardBody>
-                    <CardTitle tag="h5">{item.name}</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">$ {item.price}</CardSubtitle> 
-                    <Button>Agregar</Button>
+                  <CardBody className="cardBody" style={{padding: '7px'}}>
+                    <CardTitle tag="h6">{item.name}</CardTitle>
+                    <CardSubtitle tag="h7" className="mb-2 text-muted">$ {item.price}</CardSubtitle> 
+                    <Button style={{padding: "1px", marginTop: '4px'}}>Agregar</Button>
                   </CardBody>
                   </Card>
                 
